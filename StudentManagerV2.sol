d2a004a51ae7407e19ad33e7fd067bd258ba842a
nguyenthihuyen0212 <nthuyen0212@gmail.com>
2023-07-29 22:43:35 +0700

    update StudentManagerV2.sol file

--- StudentManagerV2.sol
+++ StudentManagerV2.sol
@@ -2,17 +2,11 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
 contract StudentManagerV2 {
-
     struct Student {
         address walletAddress;
         string name;
-        uint age;
+        uint256 age;
         string idCard;
     }
 
@@ -20,20 +14,64 @@ contract StudentManagerV2 {
     Student[] private studentRegistrationRequests;
     address private admin;
 
-
     constructor(address _admin) {
         admin = _admin;
-    }     
+    }
 
-    function register(string memory _name, uint _age, string memory _idCard) external {
+    function register(
+        string memory _name,
+        uint256 _age,
+        string memory _idCard
+    ) external {
         // Kiem tra xem student nay có đang nằm trong students hoặc studentRegistrationRequests không?
+        require(
+            studentRegistrationRequests[msg.sender].studentID == 0,
+            "Student already registered"
+        );
         Student memory student = Student(msg.sender, _name, _age, _idCard);
         studentRegistrationRequests.push(student);
     }
 
-    function listRegistrationRequests() external view returns(Student[] memory) {
+    function listRegistrationRequests()
+        external
+        view
+        returns (Student[] memory)
+    {
         // Validate chỉ admin mới được call hàm này
-        require(msg.sender == admin, 'Only admin can call this function');
+        require(msg.sender == admin, "Only admin can call this function");
         return studentRegistrationRequests;
+    }
+
+    function getDetailRegistrationRequest(address studentAddress) external view returns (address walletAddress, string memory name, uint256 age, string memory idCard) {
+
+        return(
+            studentRegistrationRequests[studentAddress].name,
+            studentRegistrationRequests[studentAddress].walletAddress,
+            studentRegistrationRequests[studentAddress].idCard,
+            studentRegistrationRequests[studentAddress].age,
+        );
+    }
+
+    function approveRequest(address studentAddress) external {
+        require(msg.sender == admin, "Only admin can approve requests");
+        return studentRegistrationRequests;
+    }
+
+    function rejectRequest(address studentAddress) external {
+        require(msg.sender == admin, "Only admin can reject requests");
+        return studentRegistrationRequests;
+    }
+
+    function remove(address studentAddress) external {
+        require(msg.sender == admin, "Only admin can remove students");
+        delete studentRegistrationRequests;
+    }
+
+    function cancel() external {
+        require(
+            studentRegistrationRequests[msg.sender].studentID != 0,
+            "Student not registered"
+        );
+        delete studentRegistrationRequests[msg.sender];
     }
 }
