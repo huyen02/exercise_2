5d1c7f21f5d9f94e7a94c35a2a6dee5bbc07bd34
nguyenthihuyen0212 <nthuyen0212@gmail.com>
2023-07-31 22:53:21 +0700

    update smartcontract_sol

--- StudentManagerV2.sol
+++ StudentManagerV2.sol
@@ -17,17 +17,28 @@ contract StudentManagerV2 {
     constructor(address _admin) {
         admin = _admin;
     }
+    function isRegisteredStudent(address studentAddress) private view returns (bool) {
+    for (uint256 i = 0; i < students.length; i++) {
+        if (students[i].walletAddress == studentAddress) {
+            return true;
+        }
+    }
+
+    for (uint256 i = 0; i < studentRegistrationRequests.length; i++) {
+        if (studentRegistrationRequests[i].walletAddress == studentAddress) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
     function register(
         string memory _name,
         uint256 _age,
         string memory _idCard
     ) external {
-        // Kiem tra xem student nay có đang nằm trong students hoặc studentRegistrationRequests không?
-        require(
-            studentRegistrationRequests[msg.sender].studentID == 0,
-            "Student already registered"
-        );
+    
         Student memory student = Student(msg.sender, _name, _age, _idCard);
         studentRegistrationRequests.push(student);
     }
@@ -43,35 +54,76 @@ contract StudentManagerV2 {
     }
 
     function getDetailRegistrationRequest(address studentAddress) external view returns (address walletAddress, string memory name, uint256 age, string memory idCard) {
-
-        return(
-            studentRegistrationRequests[studentAddress].name,
-            studentRegistrationRequests[studentAddress].walletAddress,
-            studentRegistrationRequests[studentAddress].idCard,
-            studentRegistrationRequests[studentAddress].age,
-        );
+    require(isRegisteredStudent(studentAddress), "Student not found");
+    
+    for (uint256 i = 0; i < studentRegistrationRequests.length; i++) {
+        if (studentRegistrationRequests[i].walletAddress == studentAddress) {
+            Student memory student = studentRegistrationRequests[i];
+            return (student.walletAddress, student.name, student.age, student.idCard);
+        }
     }
 
+    revert("Student not found");
+}
     function approveRequest(address studentAddress) external {
-        require(msg.sender == admin, "Only admin can approve requests");
-        return studentRegistrationRequests;
+   
+    require(msg.sender == admin, "Only admin can call this function");
+
+    // Kiểm tra xem sinh viên đã đăng ký hay chưa
+    require(isRegisteredStudent(studentAddress), "Student not found");
+
+    // Duyệt yêu cầu đăng ký và chuyển sinh viên từ danh sách yêu cầu đăng ký sang danh sách sinh viên đã đăng ký
+    for (uint256 i = 0; i < studentRegistrationRequests.length; i++) {
+        if (studentRegistrationRequests[i].walletAddress == studentAddress) {
+            students.push(studentRegistrationRequests[i]);
+            // Xóa sinh viên khỏi danh sách yêu cầu đăng ký
+            delete studentRegistrationRequests[i];
+            break;
+        }
     }
+}
 
     function rejectRequest(address studentAddress) external {
-        require(msg.sender == admin, "Only admin can reject requests");
-        return studentRegistrationRequests;
+   
+    require(msg.sender == admin, "Only admin can call this function");
+    
+    require(isRegisteredStudent(studentAddress), "Student not found");
+
+    for (uint256 i = 0; i < studentRegistrationRequests.length; i++) {
+        if (studentRegistrationRequests[i].walletAddress == studentAddress) {
+           
+            delete studentRegistrationRequests[i];
+            break;
+        }
     }
+}
 
     function remove(address studentAddress) external {
-        require(msg.sender == admin, "Only admin can remove students");
-        delete studentRegistrationRequests;
+
+    require(msg.sender == admin, "Only admin can call this function");
+
+  
+    require(isRegisteredStudent(studentAddress), "Student not found");
+
+    for (uint256 i = 0; i < students.length; i++) {
+        if (students[i].walletAddress == studentAddress) {
+           
+            delete students[i];
+            break;
+        }
     }
+}
 
     function cancel() external {
-        require(
-            studentRegistrationRequests[msg.sender].studentID != 0,
-            "Student not registered"
-        );
-        delete studentRegistrationRequests[msg.sender];
+        require(isRegisteredStudent(msg.sender), "Only registered students can call this function");
+
+        for (uint256 i = 0; i < students.length; i++) {
+            if (students[i].walletAddress == msg.sender) {
+            
+                students[i] = students[students.length - 1];
+                students.pop();
+                break;
+            }
+        }
     }
 }
